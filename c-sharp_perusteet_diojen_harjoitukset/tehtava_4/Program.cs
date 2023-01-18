3282c759dc73160f7c53567ea7d228a9885fad30
Developer250 <jani.kekalainen@hotmail.com>
2023-01-18 18:10:09 +0200

    Tehtävä 4

---
+++ c-sharp_perusteet_diojen_harjoitukset/tehtava_4/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+class prog
+{
+    public static void Main()
+    {
+        int x, b;
+        Console.WriteLine("Syötä kaksi numeroa : ");
+        x = Convert.ToInt32(Console.ReadLine());
+        b = Convert.ToInt32(Console.ReadLine());
+   
+        if (x > b)
+        {
+            Console.WriteLine("{0} Suurin numero on ", x);
+        }
+        else
+        {
+            Console.WriteLine("{0} Suurin numero on ", b);
+        }
+        Console.ReadLine();
+    }
+}
