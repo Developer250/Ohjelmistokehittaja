9a49234b2b9dbade59659a0bcf24acaeec02c83a
Developer250 <jani.kekalainen@hotmail.com>
2023-01-18 18:58:03 +0200

    tehtävät

--- c-sharp_perusteet_diojen_harjoitukset/tehtava_4/Program.cs
+++ c-sharp_perusteet_diojen_harjoitukset/tehtava_4/Program.cs
@@ -1,22 +1,42 @@
-﻿
-using System;
-class prog
+﻿internal class Program
 {
-    public static void Main()
+    private static void Main(string[] args)
     {
         int x, b;
-        Console.WriteLine("Syötä kaksi numeroa : ");
+        int c, d;
+        int e;
+        Console.WriteLine("Syötä 5 numeroa : ");
         x = Convert.ToInt32(Console.ReadLine());
         b = Convert.ToInt32(Console.ReadLine());
-   
-        if (x > b)
+        c = Convert.ToInt32(Console.ReadLine());
+        d = Convert.ToInt32(Console.ReadLine());
+        e = Convert.ToInt32(Console.ReadLine());
+
+        if (x > b && x > c && x > d && x > 3)
         {
             Console.WriteLine("{0} Suurin numero on ", x);
         }
+
+        if (b > x && b > c && b > d && b > e)
+
+            Console.WriteLine("{0} Suurin numero on ", b);
+    }
+
+            if (C > d&& C > e)
+        {
+            Console.WriteLine("{0} Suurin numero on ", c);
+        }
+
+        if (d > x && d > b && d > c && d > e)
+            (
+              Console.WriteLine("{0} Suurin numero on ", d);
+        }
+
+
         else
         {
-            Console.WriteLine("{0} Suurin numero on ", b);
+            Console.WriteLine("{0} Suurin numero on ", e);
         }
-        Console.ReadLine();
-    }
+  
 }
+//Käyttäjä syttää kaksi lukua joita verrataan if-else vertailulla ja tulostetaan suurin numero
---
+++ c-sharp_perusteet_diojen_harjoitukset/tehtava_5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class PlayWithIntDoubleAndString
+{
+    static void Main()
+    {
+        Console.WriteLine("Please choose your input data type:\n 1 --> int\n 2 --> double\n 3 --> string\n ...and hit ENTER");
+        int choice = int.Parse(Console.ReadLine());
+
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("Please enter your INTEGER number: ");
+                int a = int.Parse(Console.ReadLine());
+                Console.WriteLine("The result is: {0}", (a + 1));
+                break;
+            case 2:
+                Console.WriteLine("Please enter your DOUBLE number: ");
+                double b = double.Parse(Console.ReadLine());
+                Console.WriteLine("The result is: {0}", (b + 1));
+                break;
+            case 3:
+                Console.WriteLine("Please enter your STRING: ");
+                string c = Console.ReadLine();
+                Console.WriteLine("The result is: {0}", c + "*");
+                break;
+
+            default:
+                Console.WriteLine("Invalid!");
+                break;
+        }
+    }
+}
