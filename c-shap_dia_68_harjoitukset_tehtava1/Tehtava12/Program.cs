01ce81b5f3f3341534f7fba0628adf745fbaaa31
Developer250 <jani.kekalainen@hotmail.com>
2023-01-20 10:35:20 +0200

    tehtävät

---
+++ c-shap_dia_68_harjoitukset_tehtava1/Tehtava12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Anna suurempi kokonaisluku: "); //Pyydetään käyttäjää antamaan suurempi kokonaisluku
+        for (int i = 1; i <= 10; i++)//Tehdään 10 kertaa syötetty luku
+        {
+           
+            int num = Convert.ToInt32(Console.ReadLine());//Muuttaa syötteen kokonaisluvuksi
+            for (int j = 1; j <= num; j++)//Tehdään rivitys
+            {
+                Console.Write(num + " ");//Tulostetaan luku ja tehdään välit jokaisen luvun jälkeen
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ c-shap_dia_68_harjoitukset_tehtava1/tehtava11/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            int x = 24;
+            if (age > x)
+
+                Console.WriteLine("Your age is: " + age + " " + "näytät ikäistäsi nuoremmalta");
+            else
+                Console.WriteLine("Olet liian nuori");
+        }
+    }
+}
+
