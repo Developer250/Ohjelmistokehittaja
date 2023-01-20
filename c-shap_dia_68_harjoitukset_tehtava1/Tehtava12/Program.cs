dc0d072a7436a6f002a3592617533604167c9afc
Developer250 <jani.kekalainen@hotmail.com>
2023-01-20 10:53:51 +0200

    tehtävät

--- c-shap_dia_68_harjoitukset_tehtava1/Tehtava12/Program.cs
+++ c-shap_dia_68_harjoitukset_tehtava1/Tehtava12/Program.cs
@@ -5,15 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("Anna suurempi kokonaisluku: "); //Pyydetään käyttäjää antamaan suurempi kokonaisluku
+        
+        
         for (int i = 1; i <= 10; i++)//Tehdään 10 kertaa syötetty luku
-        {
-           
+         {
             int num = Convert.ToInt32(Console.ReadLine());//Muuttaa syötteen kokonaisluvuksi
+            
             for (int j = 1; j <= num; j++)//Tehdään rivitys
             {
-                Console.Write(num + " ");//Tulostetaan luku ja tehdään välit jokaisen luvun jälkeen
+                Console.Write(num + " ");
             }
-            Console.WriteLine();
-        }
+
+        Console.WriteLine();
+
+
+        for (int j = 1; j <= num; j++)//Tehdään rivitys
+              {
+                Console.Write(num);//Tulostetaan luku ja tehdään välit jokaisen luvun jälkeen
+                }
+            }
+
+         }
     }
-}
