1d645a14c800c09566e3815b9a3a9d790745e81f
Developer250 <jani.kekalainen@hotmail.com>
2023-01-21 13:38:54 +0200

    tehtävät

---
+++ c-shap_dia_68_harjoitukset_tehtava1/Tehtava17/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int i = 1; i <= 100; i++)//Alustetaan
+
+            if (i % 3 == 0)//Tarkistetaan onko luku jaollinen luvulla 3.
+                Console.WriteLine(i);//Tulostetaan luvulla 3 jaolliset luvut näytölle.
+    }
+}
--- c-shap_dia_68_harjoitukset_tehtava1/tehtava_16/Program.cs
+++ c-shap_dia_68_harjoitukset_tehtava1/tehtava_16/Program.cs
@@ -2,9 +2,9 @@
 {
     private static void Main(string[] args)
     {
-        for(int i = 1; i < 100; i++)
+        for(int i = 1; i < 100; i++)//Alustetaan.
 
-            if(i % 2 !=0)
-        Console.WriteLine(i);
+            if(i % 2 !=0)//Tarkistetaan onko luku pariton.
+        Console.WriteLine(i);//Tulostetaan parittomat luvut näytölle.
     }
 }
