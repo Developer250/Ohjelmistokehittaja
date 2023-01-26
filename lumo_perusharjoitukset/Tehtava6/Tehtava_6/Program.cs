3bf14902814e2d900a91f77bddd99ccc3d33fbdf
Developer250 <jani.kekalainen@hotmail.com>
2023-01-26 12:18:11 +0200

    Tehtävä

---
+++ lumo_perusharjoitukset/Tehtava6/Tehtava_6/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WindowHeight("Ole hyvä ja anna celsius luku");
+        double celsius;
+        celsius = Convert.ToDouble(Console.ReadLine());
+        double fareh = (celsius * 9) / 5 + 32;
+        Console.WriteLine(fareh);
+    }
+}
+
