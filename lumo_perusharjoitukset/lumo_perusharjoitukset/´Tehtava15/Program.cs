0f5c0e0c01b1a42bcb821698af92955b40f999e8
Developer250 <jani.kekalainen@hotmail.com>
2023-01-30 15:43:02 +0200

    tehtvä

---
+++ lumo_perusharjoitukset/lumo_perusharjoitukset/´Tehtava15/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Anna ensimmäinen luku:");  //Pyydetään käyttäjää antamaan ensimmäinen luku   
+        double a = Convert.ToDouble(Console.ReadLine());//Lisätään a-muuttujaan muuntaja
+        Console.WriteLine("Anna anna toinen luku:");//Pyydetään käyttäjää antamaan toinen luku 
+        double b = Convert.ToDouble(Console.ReadLine());//Lisätään b-muuttujaan muuntaja
+        
+        double vastaus;
+         vastaus = a - b;
+
+        Console.WriteLine(" Vastaus on:" + vastaus);
+    }
+}
