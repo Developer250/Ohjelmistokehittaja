ee422fed35703358ff9e386787992da817b2dce3
Developer250 <jani.kekalainen@hotmail.com>
2023-01-30 15:30:59 +0200

    tehtävä

---
+++ lumo_perusharjoitukset/lumo_perusharjoitukset/Tehtava14/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Anna luku farenheitina:");//Pyydetään käyttäjää antamaan luku farenheitina
+        double farenh;//Alustetaan
+        farenh = Convert.ToDouble(Console.ReadLine());//Lisätään farenh-muuttujaan muuntaja
+        double celsius = farenh - 32 / 1.8000;//Tehdään lasku
+        Console.WriteLine(celsius + " Celsiusta");//Tulostetaan celsius aste ja teksti
+    }
+}
