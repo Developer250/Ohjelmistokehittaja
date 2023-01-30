b62815e2508c73eda44d0f35cb4959ae42acbae7
Developer250 <jani.kekalainen@hotmail.com>
2023-01-30 15:10:25 +0200

    Tehtävät

---
+++ lumo_perusharjoitukset/lumo_perusharjoitukset/Tehtava11/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Anna luku senttimetreinä");//Pyydetään käyttäjää antamaan luku senttimetreinä
+        double cm;//Alustetaan
+        cm = Convert.ToInt32(Console.ReadLine());//Tallennetaan cm-muuttujaan muunnin
+
+        double tuuma = cm * 0.39370;//Tehdään itse lasku
+
+        Console.WriteLine(tuuma + " Tuumaa");//Tulostetaan laskun vastaus ja teksi
+    }
+}
---
+++ lumo_perusharjoitukset/lumo_perusharjoitukset/Tehtava12/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Anna yard-luku");//Pyydetään käyttäjää antamaan luku yardina
+        double yard;//Alustetaan
+        yard = Convert.ToInt32(Console.ReadLine());//Tallennetaan cm-muuttujaan muunnin
+        double metri = yard / 1.0936;//Tehdään itse lasku
+        Console.WriteLine( metri + " Metriä");
+    }
+}
