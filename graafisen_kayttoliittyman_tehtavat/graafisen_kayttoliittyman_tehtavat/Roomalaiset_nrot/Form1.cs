284b3c66a13ca93fbdc1b077b85e3fb98ee589b5
Developer250 <jani.kekalainen@hotmail.com>
2023-02-01 19:06:06 +0200

    Vähän alkuun tämä tehtävä

--- graafisen_kayttoliittyman_tehtavat/graafisen_kayttoliittyman_tehtavat/Roomalaiset_nrot/Form1.cs
+++ graafisen_kayttoliittyman_tehtavat/graafisen_kayttoliittyman_tehtavat/Roomalaiset_nrot/Form1.cs
@@ -18,7 +18,67 @@ namespace Roomalaiset_nrot
                 luku2 = Convert.ToInt32(tekstiTB.Text.Substring(1, 1));//Sadat
                 luku3 = Convert.ToInt32(tekstiTB.Text.Substring(2, 1));//Kympit
                 luku4 = Convert.ToInt32(tekstiTB.Text.Substring(3, 1));//Ykköset
+                if(luku1 % 3 == 0)
+                {
+                    vastaus += "MMM";
+                }
+                else if(luku1 % 2 == 0)
+                {
+                    vastaus += "MM";
+                }
+                else if(luku1 % 1 == 0)
+                {
+                    vastaus += "M";
+                }
+                else
+                {
+                    vastaus += "";
+                }
+                vastaus += sataset(luku2, vastaus); //Lähetetään mitä tuhansista tuli vastaus (M) ja lisätään vastaukseen mitä satasesta tuli (C)
             }
         }
-    }
+          private string sataset (int luku2, string vastaus)
+          { 
+             if(luku2 % 9 == 0 && luku2 != 0)
+             {
+                return "CM";//900
+             }
+             else if(luku2 % 8 == 0 && luku2 != 0)
+             {
+                return "DCCC";//800
+             }
+            else if (luku2 % 7 == 0 && luku2 != 0)
+            {
+                return "DCC";//700
+            }
+            else if (luku2 % 6 == 0 && luku2 != 0)
+            {
+                return "DC";//600
+            }
+            else if (luku2 % 5 == 0 && luku2 != 0)
+            {
+                return "D";//500
+            }
+            else if (luku2 % 4 == 0 && luku2 != 0)
+            {
+                return "CD";//400
+            }
+            else if (luku2 % 3 == 0 && luku2 != 0)
+            {
+                return "CCC";//300
+            }
+            else if (luku2 % 2 == 0 && luku2 != 0)
+            {
+                return "CC";//200
+            }
+            else if (luku2 % 1 == 0 && luku2 != 0)
+            {
+                return "C";//100
+            }
+            else 
+            {
+                return "";//Muutoin tyhjä
+            }
+        }
+    }        
 }
