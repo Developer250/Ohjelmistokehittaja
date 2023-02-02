70dd27901091fbf458eb23d155b02690a802bde5
Developer250 <jani.kekalainen@hotmail.com>
2023-02-02 11:12:24 +0200

    tehtävä

--- graafisen_kayttoliittyman_tehtavat/graafisen_kayttoliittyman_tehtavat/BMI-laskuri/Form1.Designer.cs
+++ graafisen_kayttoliittyman_tehtavat/graafisen_kayttoliittyman_tehtavat/BMI-laskuri/Form1.Designer.cs
@@ -30,10 +30,12 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.painotTB = new System.Windows.Forms.TextBox();
+            this.painoTB = new System.Windows.Forms.TextBox();
             this.pituusTB = new System.Windows.Forms.TextBox();
             this.laskeBT = new System.Windows.Forms.Button();
             this.vastausLB = new System.Windows.Forms.Label();
+            this.kuvausLB = new System.Windows.Forms.Label();
+            this.kuvainnollinenLB = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -54,12 +56,12 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "Anna pituus:";
             // 
-            // painotTB
+            // painoTB
             // 
-            this.painotTB.Location = new System.Drawing.Point(240, 40);
-            this.painotTB.Name = "painotTB";
-            this.painotTB.Size = new System.Drawing.Size(125, 38);
-            this.painotTB.TabIndex = 2;
+            this.painoTB.Location = new System.Drawing.Point(240, 40);
+            this.painoTB.Name = "painoTB";
+            this.painoTB.Size = new System.Drawing.Size(125, 38);
+            this.painoTB.TabIndex = 2;
             // 
             // pituusTB
             // 
@@ -76,6 +78,7 @@
             this.laskeBT.TabIndex = 4;
             this.laskeBT.Text = "Laske painoindeksi";
             this.laskeBT.UseVisualStyleBackColor = true;
+            this.laskeBT.Click += new System.EventHandler(this.laskeBT_Click);
             // 
             // vastausLB
             // 
@@ -87,19 +90,39 @@
             this.vastausLB.Text = "Vastaus";
             this.vastausLB.Visible = false;
             // 
+            // kuvausLB
+            // 
+            this.kuvausLB.AutoSize = true;
+            this.kuvausLB.Location = new System.Drawing.Point(66, 292);
+            this.kuvausLB.Name = "kuvausLB";
+            this.kuvausLB.Size = new System.Drawing.Size(0, 31);
+            this.kuvausLB.TabIndex = 6;
+            // 
+            // kuvainnollinenLB
+            // 
+            this.kuvainnollinenLB.AutoSize = true;
+            this.kuvainnollinenLB.Location = new System.Drawing.Point(65, 293);
+            this.kuvainnollinenLB.Name = "kuvainnollinenLB";
+            this.kuvainnollinenLB.Size = new System.Drawing.Size(86, 31);
+            this.kuvainnollinenLB.TabIndex = 7;
+            this.kuvainnollinenLB.Text = "Kuvaus";
+            this.kuvainnollinenLB.Visible = false;
+            // 
             // BMIForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(387, 281);
+            this.ClientSize = new System.Drawing.Size(585, 419);
+            this.Controls.Add(this.kuvainnollinenLB);
+            this.Controls.Add(this.kuvausLB);
             this.Controls.Add(this.vastausLB);
             this.Controls.Add(this.laskeBT);
             this.Controls.Add(this.pituusTB);
-            this.Controls.Add(this.painotTB);
+            this.Controls.Add(this.painoTB);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "BMIForm";
             this.Text = "Painoindeksilaskuri";
             this.ResumeLayout(false);
@@ -111,9 +134,11 @@
 
         private Label label1;
         private Label label2;
-        private TextBox painotTB;
+        private TextBox painoTB;
         private TextBox pituusTB;
         private Button laskeBT;
         private Label vastausLB;
+        private Label kuvausLB;
+        private Label kuvainnollinenLB;
     }
 }
--- graafisen_kayttoliittyman_tehtavat/graafisen_kayttoliittyman_tehtavat/BMI-laskuri/Form1.cs
+++ graafisen_kayttoliittyman_tehtavat/graafisen_kayttoliittyman_tehtavat/BMI-laskuri/Form1.cs
@@ -6,5 +6,49 @@ namespace BMI_laskuri
         {
             InitializeComponent();
         }
+
+        private void laskeBT_Click(object sender, EventArgs e)
+        {
+            double paino = 0, pituus = 0;
+            paino = Convert.ToDouble(painoTB.Text);
+            pituus = Convert.ToDouble(pituusTB.Text);
+            double BMI = Math.Round(paino / (paino * pituus), 2);//Pyöristetään 2-desimaalin arvoon painoinkdeksi
+
+            if(BMI < 18.5)//Tarkistetaan painoindeksi
+            {
+                vastausLB.Text = "Painoindeksisi on " + BMI;//Vastaukseen sijoitetaan teksti ja itse painoindeksi
+                vastausLB.ForeColor = Color.Blue;//Vastaus tekstin väri vesi
+                kuvainnollinenLB.Text = "Alipaino";//Kuvaus labeliin teksti
+                kuvainnollinenLB.ForeColor = Color.Blue;//Kuvaus labelin tekstin väri
+                kuvainnollinenLB.Visible = true;//Tulostetaan näkyviin
+            }
+            else if(BMI < 25)
+            {
+                vastausLB.Text = "Painoindeksisi on: " + BMI;//Vastaukseen sijoitetaan teksti ja itse painoindeksi
+                vastausLB.ForeColor = Color.Green;//Vastaus tekstin väri vesi
+                kuvainnollinenLB.Text = "Normaalipaino";//Kuvaus labeliin teksti
+                kuvainnollinenLB.ForeColor = Color.Green;//Kuvaus labelin tekstin väri
+                vastausLB.Visible = true;
+                kuvainnollinenLB.Visible = true;//Tulostetaan näkyviin
+            }
+            if(BMI < 40)
+            {
+                vastausLB.Text = "Painoindeksisi on: " + BMI;//Vastaukseen sijoitetaan teksti ja itse painoindeksi
+                vastausLB.ForeColor = Color.Gold;//Vastaus tekstin väri vesi
+                kuvainnollinenLB.Text = "Ylipaino";//Kuvaus labeliin teksti
+                kuvainnollinenLB.ForeColor = Color.Gold;//Kuvaus labelin tekstin väri
+                vastausLB.Visible = true;
+                kuvainnollinenLB.Visible = true;//Tulostetaan näkyviin
+            }
+            else
+            {
+                vastausLB.Text = "Painoindeksisi on: " + BMI;//Vastaukseen sijoitetaan teksti ja itse painoindeksi
+                vastausLB.ForeColor = Color.Red;//Vastaus tekstin väri vesi
+                kuvainnollinenLB.Text = "Huomattavasti ylipaino";//Kuvaus labeliin teksti
+                kuvainnollinenLB.ForeColor = Color.Red;//Kuvaus labelin tekstin väri
+                vastausLB.Visible = true;
+                kuvainnollinenLB.Visible = true;//Tulostetaan näkyviin
+            }
+        }
     }
 }
