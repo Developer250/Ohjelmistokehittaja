744b49573e9269d1aa6c2aee3ad27e9cf6564b4f
Developer250 <jani.kekalainen@hotmail.com>
2023-02-03 10:21:13 +0200

    harjoitukset

---
+++ graafisen_kayttoliittyman_tehtavat/Lumoakatemia_IF _ ELSE_HARJOITUKSIA/tehtava_3/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Anna luku");
+        double vastaus;
+        vastaus  = Convert.ToDouble(Console.ReadLine());
+        //ALustetaan luku1 ja luku2 sekä vastaus
+        if (vastaus <= 0 && vastaus != 0)//Tarkistetaan onko annettu luku pienempi kuin nolla ja samanaikaisesti ei ole nolla
+        {
+            Console.WriteLine("Luku on negatiivinen " + vastaus);
+        }
+
+       else if (vastaus >= 0 && vastaus != 0)//Tarkistetaan onko annettu luku suurempi kuin nolla ja samanaikaisesti ei ole nolla
+        {
+            Console.WriteLine("Luku on positiivinen " + vastaus);
+       }
+        else if (vastaus == 0)
+        {
+            Console.WriteLine("Luku on nolla");//Tarkistetaan onko annettu luku nolla
+        }
+    }
+}
