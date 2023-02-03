f90177deaf23e47d370fcdd91020aae04c717ee5
Developer250 <jani.kekalainen@hotmail.com>
2023-02-03 09:14:50 +0200

    harjoitukset

---
+++ graafisen_kayttoliittyman_tehtavat/Lumoakatemia_IF _ ELSE_HARJOITUKSIA/Tehtava1/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double luku1 = 20;
+        double luku2 = 14;
+        if (luku1 > luku2)
+        {
+            Console.WriteLine("Suurin luku on : " + luku1);
+        }
+        else
+        {
+            Console.WriteLine("Suurin luku on : " + luku2);
+        }
+    }
+}
---
+++ graafisen_kayttoliittyman_tehtavat/Lumoakatemia_IF _ ELSE_HARJOITUKSIA/Tehtava2/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double luku1 = 100; double luku2 = 54.7; double luku3 = 10;
+        if(luku1 > luku2 && luku1 > luku3)
+        {
+            Console.WriteLine(" Suurin luku on:" + luku1);
+        }
+        else if (luku2 > luku3 && luku2 > luku1)
+        {
+            Console.WriteLine(" Suurin luku on:" + luku2);
+        }
+        else
+        {
+            Console.WriteLine(" Suurin luku on:" + luku3);
+        }
+    }
+}
