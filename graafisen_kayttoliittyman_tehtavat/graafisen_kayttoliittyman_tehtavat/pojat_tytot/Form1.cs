f2b99be9747b41ef48612c8b51536343bc2472ac
Developer250 <jani.kekalainen@hotmail.com>
2023-02-09 09:26:32 +0200

    Kommentit

--- graafisen_kayttoliittyman_tehtavat/graafisen_kayttoliittyman_tehtavat/pojat_tytot/Form1.cs
+++ graafisen_kayttoliittyman_tehtavat/graafisen_kayttoliittyman_tehtavat/pojat_tytot/Form1.cs
@@ -19,17 +19,17 @@ namespace pojat_tytot
             vastausLB.Text = "";//Tyhjennetään vastaus joka kerta
             vastausLB.Visible = false;
 
-            string[] pojat = File.ReadAllLines("C:\\temp\\pojat.txt");
-            string[] tytot = File.ReadAllLines("C:\\temp\\tytot.txt");
-            string nimi = nimiTB.Text;
-            int laskurip = 1; int laskurit = 1;
+            string[] pojat = File.ReadAllLines("C:\\temp\\pojat.txt");//Luetaan teksti taulukkoon, jotta voidaan verrata
+            string[] tytot = File.ReadAllLines("C:\\temp\\tytot.txt");//Luetaan teksti taulukkoon, jotta voidaan verrata
+            string nimi = nimiTB.Text;//Haetaan tekstiboksi nimi-muuttujaan
+            int laskurip = 1; int laskurit = 1;//Tehdään molemmille sukupuolillle laskurit
 
             foreach(string poju in pojat)
             {
-                if(nimi == poju)
+                if(nimi == poju)//Tarkistetaan, jos käyttäjän antama nimi on poju-nimestä 
                 {
-                    vastausLB.Text = "Nimesi on " + laskurip + ".suosituin poikien nimi vuonna 2023 ";
-                    vastausLB.Visible = true;
+                    vastausLB.Text = "Nimesi on " + laskurip + ".suosituin poikien nimi vuonna 2023 ";//Haetaan teksti ja laskuri etsii monnenesko nimi on 
+                    vastausLB.Visible = true;//Tulostetaan vastaus esille
                 }
                 laskurip++;
             }
@@ -37,14 +37,14 @@ namespace pojat_tytot
             {
                 if (nimi == tytto)
                 {
-                    vastausLB.Text = "Nimesi on " + laskurit + ".suosituin tyttöjen nimi vuonna 2023 ";
-                    vastausLB.Visible = true;
+                    vastausLB.Text = "Nimesi on " + laskurit + ".suosituin tyttöjen nimi vuonna 2023 ";//Haetaan teksti ja laskuri etsii monnenesko nimi on 
+                    vastausLB.Visible = true;//Tulostetaan vastaus esille
                 }
                 laskurit++;
             }
             if (vastausLB.Visible == false)
-                vastausLB.Text = "Nimeäsi ei löytynyt suosituimpien nimien joukosta!";
-                 vastausLB.Visible = true;
+                vastausLB.Text = "Nimeäsi ei löytynyt suosituimpien nimien joukosta!";//Haetaan teksti, jos nimi ei ole listassas
+                 vastausLB.Visible = true;//Tulostetaan vastaus esille
         }
     }
 }
