858404f65d6eea4593b96ad787cc0ca5a805f187
Developer250 <jani.kekalainen@hotmail.com>
2023-02-09 09:36:11 +0200

    Valmis

--- graafisen_kayttoliittyman_tehtavat/graafisen_kayttoliittyman_tehtavat/Paivakirja/Form1.cs
+++ graafisen_kayttoliittyman_tehtavat/graafisen_kayttoliittyman_tehtavat/Paivakirja/Form1.cs
@@ -7,20 +7,20 @@ namespace Paivakirja
         public paivakirjaForm()
         {
             InitializeComponent();
-            string teksti = File.ReadAllText("C:\\temp\\paivakirja.txt");
-            SyottoTB.Text = teksti;
+            string teksti = File.ReadAllText("C:\\temp\\paivakirja.txt");//Luodaan polku teksti-muuttujaan
+            SyottoTB.Text = teksti;//Haetaan tekstiboksiin polku
 
         }
 
         private void TallennaBT_Click(object sender, EventArgs e)
         {
             string teksti = ""; //Luetaan teksti, jotta ei mene kirjoitus tekstin p‰‰lle, vaan per‰‰n
-            teksti += SyottoTB.Text;//Haetaan teksti
-            teksti += " " + DateTime.Now.ToString("dd.MM.yyyy HH:mm") + "\n";//Lis‰t‰‰n v‰lilyˆnti tekstin ja p‰iv‰m‰‰r‰n v‰lille
-            TextWriter text = new StreamWriter("C:\\temp\\paivakirja.txt");
-            text.Write(teksti);//Pystyt‰‰n kirjoittamaan t‰ll‰ metodilla
-            text.Close();//uljetaan p‰iv‰kirja
-            Application.Exit();
+            teksti += SyottoTB.Text;//Haetaan teksti-muuttujaan tekstiboksin teksti, jonka k‰ytt‰j‰ syˆtt‰‰
+            teksti += " " + DateTime.Now.ToString("dd.MM.yyyy HH:mm") + "\n";//Lis‰t‰‰n v‰lilyˆnti tekstin ja p‰iv‰m‰‰r‰n v‰lille sek‰ enter painallus ("/n")
+            TextWriter text = new StreamWriter("C:\\temp\\paivakirja.txt");//Luodaan polku
+            text.Write(teksti);//Pystyt‰‰n kirjoittamaan t‰ll‰ metodilla teksti‰ p‰iv‰kirjaan
+            text.Close();//Suljetaan p‰iv‰kirja
+            Application.Exit();//Poistutaans ovelluksesta
         }
     }
 }
