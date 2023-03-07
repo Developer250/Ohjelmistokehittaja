6d348e750bd3be8805b7d98789456da092c39a3e
Developer250 <jani.kekalainen@hotmail.com>
2023-03-07 14:21:41 +0200

    hotelli

---
+++ graafisen_kayttoliittyman_projektit/Projektit/Hotelli/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Hotelli
+{
+    public partial class Kirjautumisikkuna : Form
+    {
+        public Kirjautumisikkuna()
+        {
+            InitializeComponent();
+        }
+
+        private void KirjauduPainike_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
