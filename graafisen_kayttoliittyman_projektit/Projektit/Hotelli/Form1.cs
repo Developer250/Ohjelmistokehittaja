d88745fecef453aef9db0c1a8eb7a8e5ad873e6a
Developer250 <jani.kekalainen@hotmail.com>
2023-03-07 15:39:39 +0200

    Kirjautumisikkuna valmis

--- graafisen_kayttoliittyman_projektit/Projektit/Hotelli/Form1.cs
+++ graafisen_kayttoliittyman_projektit/Projektit/Hotelli/Form1.cs
@@ -27,7 +27,45 @@ namespace Hotelli
             MySqlDataAdapter adapteri = new MySqlDataAdapter();
             MySqlCommand komento = new MySqlCommand();
             //Tehdään kysely
-            String kysely = "SELECT kayttajanimi, salasana FROM asiakkaat WHERE kayttajanimi = @kn AND salasana = @ss";
+            String kysely = "SELECT kayttajanimi, salasana FROM asiakkaat WHERE Kayttajanimi = @kn AND Salasana = @ss";
+
+            komento.CommandText = kysely;
+            komento.Connection = tietokantaan.otaYhteys();
+
+            //Lisätään lomakkeelta muuttujiin kn ja @ss tiedot
+            komento.Parameters.Add("@kn", MySqlDbType.VarChar).Value = KayttajaTB.Text;
+            komento.Parameters.Add("@ss", MySqlDbType.VarChar).Value = SalasanaTB.Text;
+
+            adapteri.SelectCommand = komento;
+            adapteri.Fill(taulu);
+            //Tarkistetaan, onko käyttäjänimi ja salasana tietokannassa
+            //Eli jos tulee, löytyy yhdistelmä tietokannasta
+
+            if(taulu.Rows.Count > 0)
+            {
+                //Piilotetaan tämä lomake ja avataan pääikkuna
+                this.Hide();
+                Paaikkuna plomake = new Paaikkuna();
+                plomake.Show();
+
+                //MessageBox.Show("Tietoja löytyy);
+            }
+            //Muussa tapauksessa tarkistetaan, onko jompikumpi kenttä tyhjä
+            else
+            {
+                if (KayttajaTB.Text.Trim().Equals(""))
+                {
+                    MessageBox.Show("Syötä käyttäjänimesi kirjautuaksesi", "Käyttäjänimi on tyhjä", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (SalasanaTB.Text.Trim().Equals(""))
+                {
+                    MessageBox.Show("Syötä salasana", "Salasana on tyhjä", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Käyttäjänimeä tai salasanaa ei löytynyt", "tietoja ei löydy", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
