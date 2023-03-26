43e37e3b8673fca38c392bf91d60905892e4e57b
Developer250 <jani.kekalainen@hotmail.com>
2023-03-26 17:36:18 +0300

    Luotu varaustietokanta ja desing tehty

--- graafisen_kayttoliittyman_projektit/Hotellivarausjarjestelma/Hotellivarausjarjestelma/RESERVATION.cs
+++ graafisen_kayttoliittyman_projektit/Hotellivarausjarjestelma/Hotellivarausjarjestelma/RESERVATION.cs
@@ -1,13 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace Hotellivarausjarjestelma
 {
-    internal class RESERVATION
+    class RESERVATION
     {
+        CONNECT conn = new CONNECT();
+        public DataTable getAllReserv()
+        {
+            MySqlCommand command = new MySqlCommand("SELCET *  FROM `reservations`", conn.getConnection());
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            DataTable table = new DataTable();
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            return table;
+        }
 
     }
 }
