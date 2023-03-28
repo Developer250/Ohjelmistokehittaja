b49890388c01ec0fce1ca0fccabf0d9a6d421beb
Developer250 <jani.kekalainen@hotmail.com>
2023-03-28 19:32:47 +0300

    kesken

--- graafisen_kayttoliittyman_projektit/Hotellivarausjarjestelma/Hotellivarausjarjestelma/RESERVATION.cs
+++ graafisen_kayttoliittyman_projektit/Hotellivarausjarjestelma/Hotellivarausjarjestelma/RESERVATION.cs
@@ -22,6 +22,81 @@ namespace Hotellivarausjarjestelma
 
             return table;
         }
+        public bool addReservation(int number, int clientId, DateTime dateIN, DateTime dateOUT)
+        {
+            MySqlCommand command = new MySqlCommand();
+            String insertQuery = "INSERT INTO `reservations`(`roomNumber`, `clientid`, `DateIn`, `DateOut`) VALUES (@rnm, @cid, @din, @dout)";
+            command.CommandText = insertQuery;
+            command.Connection = conn.getConnection();
 
+            //(@rnm, @cid, @din, @dout
+            command.Parameters.Add("@rnm", MySqlDbType.Int32).Value = number;
+            command.Parameters.Add("@cid", MySqlDbType.Int32).Value = clientId;
+            command.Parameters.Add("@din", MySqlDbType.Date).Value = dateIN;
+            command.Parameters.Add("@dout", MySqlDbType.Date).Value = dateOUT;
+
+            conn.openConnection();
+
+            if (command.ExecuteNonQuery() == 1)
+            {
+                conn.closeConnection();
+                return true;
+            }
+            else
+            {
+                conn.closeConnection();
+                return false;
+            }
+        }
+        public bool editReserv(int number, int clientId,int reservId, DateTime dateIN, DateTime dateOUT)
+        {
+            MySqlCommand command = new MySqlCommand();
+            String editQuery = "UPDATE `reservations` SET `roomNumber`=@rnm ,`clientid`=@cid ,`DateIn`=@din ,`DateOut`= @dout WHERE  `reservid`=@rvid";
+            command.CommandText = editQuery;
+            command.Connection = conn.getConnection();
+
+            //(@rnm, @cid, @din, @dout,@rvid
+            command.Parameters.Add("@rvid", MySqlDbType.Int32).Value = reservId;
+            command.Parameters.Add("@rnm", MySqlDbType.Int32).Value = number;
+            command.Parameters.Add("@cid", MySqlDbType.Int32).Value = clientId;
+            command.Parameters.Add("@din", MySqlDbType.Date).Value = dateIN;
+            command.Parameters.Add("@dout", MySqlDbType.Date).Value = dateOUT;
+
+            conn.openConnection();
+
+            if (command.ExecuteNonQuery() == 1)
+            {
+                conn.closeConnection();
+                return true;
+            }
+            else
+            {
+                conn.closeConnection();
+                return false;
+            }
+        }
+        public bool removeReserv(int rsv_id)
+        {
+            MySqlCommand command = new MySqlCommand();
+            String removeQuery = "DELETE FROM `reservations` WHERE `reservid`=@rvid";
+            command.CommandText = removeQuery;
+            command.Connection = conn.getConnection();
+
+            //@number
+            command.Parameters.Add("@rvid", MySqlDbType.Int32).Value = rsv_id;
+
+            conn.openConnection();
+
+            if (command.ExecuteNonQuery() == 1)
+            {
+                conn.closeConnection();
+                return true;
+            }
+            else
+            {
+                conn.closeConnection();
+                return false;
+            }
+        }
     }
 }
