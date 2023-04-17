7b2a1ed1cb53e4c2c62856df75b9102912fe8cb0
Developer250 <jani.kekalainen@hotmail.com>
2023-04-17 16:08:48 +0300

    jatka kirjautumisikkunaa

---
+++ graafisen_kayttoliittyman_projektit/EmplyeeManagementSystem/EmplyeeManagementSystem/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace EmplyeeManagementSystem
+{
+    partial class loginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Username = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.signin_btn = new System.Windows.Forms.Button();
+            this.close_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(171, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ready To Login";
+            // 
+            // Username
+            // 
+            this.Username.AutoSize = true;
+            this.Username.Location = new System.Drawing.Point(171, 163);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(73, 16);
+            this.Username.TabIndex = 1;
+            this.Username.Text = "Username:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password:";
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(262, 163);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(100, 22);
+            this.textBoxUsername.TabIndex = 3;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(262, 214);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(100, 22);
+            this.textBoxPassword.TabIndex = 4;
+            // 
+            // signin_btn
+            // 
+            this.signin_btn.Location = new System.Drawing.Point(174, 282);
+            this.signin_btn.Name = "signin_btn";
+            this.signin_btn.Size = new System.Drawing.Size(98, 23);
+            this.signin_btn.TabIndex = 5;
+            this.signin_btn.Text = "Sign In";
+            this.signin_btn.UseVisualStyleBackColor = true;
+            this.signin_btn.Click += new System.EventHandler(this.signin_btn_Click);
+            // 
+            // close_btn
+            // 
+            this.close_btn.Location = new System.Drawing.Point(459, 12);
+            this.close_btn.Name = "close_btn";
+            this.close_btn.Size = new System.Drawing.Size(32, 25);
+            this.close_btn.TabIndex = 6;
+            this.close_btn.Text = "x";
+            this.close_btn.UseVisualStyleBackColor = true;
+            this.close_btn.Click += new System.EventHandler(this.close_btn_Click);
+            // 
+            // loginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.ClientSize = new System.Drawing.Size(503, 388);
+            this.Controls.Add(this.close_btn);
+            this.Controls.Add(this.signin_btn);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.label1);
+            this.Name = "loginForm";
+            this.Text = "LoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Username;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button signin_btn;
+        private System.Windows.Forms.Button close_btn;
+    }
+}
+
---
+++ graafisen_kayttoliittyman_projektit/EmplyeeManagementSystem/EmplyeeManagementSystem/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmplyeeManagementSystem
+{
+    public partial class loginForm : Form
+    {
+        public loginForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void close_btn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void signin_btn_Click(object sender, EventArgs e)
+        {
+            string myconnectionString = @"server=localhost;database=ems_system;userid=root;password=;";
+            MySqlConnection con = null;
+            MySqlCommand cmd;
+            MySqlDataReader dr;
+
+            try
+            {
+                con = new MySqlConnection(myconnectionString);
+                con.Open();
+                cmd = new MySqlCommand();
+                cmd.Connection = con;
+                con.Command.Text = "SELECT * FROM login_tb WHERE Username='"+ textBoxUsername.Text+"' AND Password='"+textBoxPassword.Text+"' ";
+                dr = cmd.ExecuteReader();
+                if (dr.Reade())
+                {
+                    MessageBox.Show("Succes");
+                }
+                else
+                {
+                    MessageBox.Show("Please enter the correct usernnme and password");
+                }
+            }
+            finally
+            {
+                if (con != null)
+                {
+                    con.Close();
+                }
+            }
+        }
+    }
+}
